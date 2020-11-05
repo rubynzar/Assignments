711536036a282db351243a37940a5d2e84bafae6
rubynzar <57731427+rubynzar@users.noreply.github.com>
2020-11-05 11:12:34 +0100

    Update js.js

--- assign 03/js.js
+++ assign 03/js.js
@@ -1,4 +1,5 @@
 let dice= document.getElementsByClassName('dice');
+var diceArray=[];
 
 const roll = function (foo) {
     return Math.floor(Math.random() * foo) + 1;
@@ -7,5 +8,8 @@ const roll = function (foo) {
 for (var i = 0; i < dice.length;) {
   let x = roll(6);
   dice[i].innerHTML = x;
+  diceArray.push(x);
   i++;
 }
+
+console.log(diceArray);
